9d31cdd8e891aa93d72ca7a61d3f82aae0ddfba6
Linh Nguyen <nh0xtunk15@gmail.com>
2022-09-08 20:55:44 +0700

    added message on running

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var express = require("express");
 var cors = require("cors");
 var app = express();
-var bodyParser = require("body-parser")
-var server = require("http").Server(app)
-app.set("view engine","ejs");
-app.set("views","./views")
+var bodyParser = require("body-parser");
+var server = require("http").Server(app);
+
+const PORT = 3000
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
 app.use(express.static("public"));
 app.use(cors({ credentials: true, origin: true }));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-server.listen(3000);
-app.get("/", function(req,res){
+server.listen(PORT, () => {
+  console.log("Running in port " + PORT);
+});
+app.get("/", function (req, res) {
   res.render("home");
-})
-
+});
